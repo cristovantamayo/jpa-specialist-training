a72dbebb2a32ce53384adef4552dfd822789a70e
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-03 20:35:43 -0300

    Persistence.xml added

--- src/main/java/dev/cristovantamayo/ecommerce/model/Produto.java
+++ src/main/java/dev/cristovantamayo/ecommerce/model/Produto.java
@@ -1,4 +1,26 @@
 package dev.cristovantamayo.ecommerce.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+
+@Entity
 public class Produto {
+
+    @Id
+    private Integer id;
+
+    private String nome;
+
+    private String descricao;
+
+    public BigDecimal preco;
 }
