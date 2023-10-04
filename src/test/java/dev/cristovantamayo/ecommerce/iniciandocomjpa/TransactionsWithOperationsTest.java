a1865e678b0ab03dee13a35c52947068f306c152
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-04 15:43:07 -0300

    Test Added

--- src/test/java/dev/cristovantamayo/ecommerce/iniciandocomjpa/TransactionsWithOperationsTest.java
+++ src/test/java/dev/cristovantamayo/ecommerce/iniciandocomjpa/TransactionsWithOperationsTest.java
@@ -2,8 +2,32 @@ package dev.cristovantamayo.ecommerce.iniciandocomjpa;
 
 import dev.cristovantamayo.ecommerce.EntityManagerTest;
 import dev.cristovantamayo.ecommerce.model.Produto;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
 
 public class TransactionsWithOperationsTest extends EntityManagerTest {
+
+    @Test
+    public void insertFirstObject() {
+        Produto product = new Produto();
+        product.setId(2);
+        product.setNome("Câmera Canon");
+        product.setDescricao("A melhor definição para suas Fotos.");
+        product.setPreco(new BigDecimal(5000));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(product);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto actualProduct =
+                entityManager.find(Produto.class, product.getId());
+
+        Assert.assertNotNull(actualProduct);
+    }
 
     public void openAndCloseTransaction() {
 
