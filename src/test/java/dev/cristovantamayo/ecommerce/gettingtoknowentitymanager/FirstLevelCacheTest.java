4736845c2ebc6d6440bc43b3602e996bd54736c3
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-06 07:32:25 -0300

    Cache Test Validated

---
+++ src/test/java/dev/cristovantamayo/ecommerce/gettingtoknowentitymanager/FirstLevelCacheTest.java
@@ -0,0 +1,19 @@
+package dev.cristovantamayo.ecommerce.gettingtoknowentitymanager;
+
+import dev.cristovantamayo.ecommerce.EntityManagerTest;
+import dev.cristovantamayo.ecommerce.model.Product;
+import org.junit.Test;
+
+public class FirstLevelCacheTest extends EntityManagerTest {
+
+    @Test
+    public void cacheValidation() {
+
+        Product product = entityManager.find(Product.class, 1);
+        System.out.println(product.getName());
+        System.out.println("------------------------------------");
+        Product recoveredProduct = entityManager.find(Product.class, product.getId());
+        System.out.println(recoveredProduct.getName());
+
+    }
+}
