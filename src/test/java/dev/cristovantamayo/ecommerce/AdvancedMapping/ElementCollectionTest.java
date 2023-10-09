2e268f1fff20e8dc8ac25f04a66c7e356a8bd69b
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-09 08:43:05 -0300

    Embeddeble Object Element Collection Validated

--- src/test/java/dev/cristovantamayo/ecommerce/AdvancedMapping/ElementCollectionTest.java
+++ src/test/java/dev/cristovantamayo/ecommerce/AdvancedMapping/ElementCollectionTest.java
@@ -1,6 +1,7 @@
 package dev.cristovantamayo.ecommerce.AdvancedMapping;
 
 import dev.cristovantamayo.ecommerce.EntityManagerTest;
+import dev.cristovantamayo.ecommerce.model.Attribute;
 import dev.cristovantamayo.ecommerce.model.Product;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -22,5 +23,20 @@ public class ElementCollectionTest extends EntityManagerTest {
 
         Product actualProduct = entityManager.find(Product.class, product.getId());
         Assertions.assertFalse(actualProduct.getTags().isEmpty());
+    }
+
+    @Test
+    public void applyAttributes() {
+        entityManager.getTransaction().begin();
+
+        Product product = entityManager.find(Product.class, 1);
+        product.setAttributes(Arrays.asList(Attribute.of("screen", "12 pol")));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Product actualProduct = entityManager.find(Product.class, product.getId());
+        Assertions.assertFalse(actualProduct.getAttributes().isEmpty());
     }
 }
