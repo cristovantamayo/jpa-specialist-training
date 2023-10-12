0c5b3ee3096c037322912f20b283ec7eb161381b
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-12 13:38:41 -0300

    Use Join with JPQL

---
+++ src/test/java/dev/cristovantamayo/ecommerce/jpql/JoinTest.java
@@ -0,0 +1,43 @@
+package dev.cristovantamayo.ecommerce.jpql;
+
+import dev.cristovantamayo.ecommerce.EntityManagerTest;
+import dev.cristovantamayo.ecommerce.model.Product;
+import dev.cristovantamayo.ecommerce.model.Purchase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.Arrays;
+import java.util.List;
+
+public class JoinTest extends EntityManagerTest {
+
+    @Test
+    public void doJoin() {
+
+        final String jpqlJoin = "select p From Purchase p join p.payment pg";
+        TypedQuery<Purchase> typedQueryJoin = entityManager.createQuery(jpqlJoin, Purchase.class);
+        List<Purchase> purchasesJoin = typedQueryJoin.getResultList();
+        Assertions.assertTrue(purchasesJoin.size() == 1);
+
+        final String jpql = "select p From Purchase p";
+        TypedQuery<Purchase> typedQuery = entityManager.createQuery(jpql, Purchase.class);
+        List<Purchase> purchases = typedQuery.getResultList();
+        // Assertions.assertTrue(purchases.size() == 1); // without join fails
+
+        /** Advantage */
+        final String jpqlAdv= "select p, i from Purchase p join p.purchaseItems i";
+        TypedQuery<Object[]> typedQueryAdv = entityManager.createQuery(jpqlAdv, Object[].class);
+        List<Object[]> purchasesAdv = typedQueryAdv.getResultList();
+        System.out.println("results: " + purchasesAdv.size());
+        Assertions.assertTrue(purchasesAdv.size() == 3);
+
+        /** Advantage 2 */
+        final String jpqlAdv2= "select pro from Purchase p join p.purchaseItems i join i.product pro";
+        TypedQuery<Product> typedQueryAdv2 = entityManager.createQuery(jpqlAdv2, Product.class);
+        List<Product> products = typedQueryAdv2.getResultList();
+        products.forEach(p -> System.out.println(p.getName()));
+        Assertions.assertTrue(products.size() == 3);
+
+    }
+}
