64ea68b6109c5e567a97254f96ce54c0680cdfdd
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-12 11:00:57 -0300

    Respons with inner Entity as Result without Join

--- src/test/java/dev/cristovantamayo/ecommerce/jpql/BasicJPQJTest.java
+++ src/test/java/dev/cristovantamayo/ecommerce/jpql/BasicJPQJTest.java
@@ -1,6 +1,8 @@
 package dev.cristovantamayo.ecommerce.jpql;
 
 import dev.cristovantamayo.ecommerce.EntityManagerTest;
+import dev.cristovantamayo.ecommerce.model.Client;
+import dev.cristovantamayo.ecommerce.model.Product;
 import dev.cristovantamayo.ecommerce.model.Purchase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -45,5 +47,20 @@ public class BasicJPQJTest extends EntityManagerTest {
         Assertions.assertNotNull(purchase2);
         List<Purchase> purchases = query.getResultList();
         Assertions.assertFalse(purchases.isEmpty());
+    }
+
+    @Test
+    public void selectAttributeAsResult() {
+
+        final String jpql = "select p.name from Product p";
+
+        TypedQuery<String> typedQuery = entityManager.createQuery(jpql, String.class);
+        List<String> purchasesNames = typedQuery.getResultList();
+        Assertions.assertTrue(String.class.equals(purchasesNames.get(0).getClass()));
+
+        final String jpqlClient = "select p.client from Purchase p";
+        TypedQuery<Client> typedQueryClient = entityManager.createQuery(jpqlClient, Client.class);
+        List<Client> clients = typedQueryClient.getResultList();
+        Assertions.assertTrue(Client.class.equals(clients.get(0).getClass()));
     }
 }
