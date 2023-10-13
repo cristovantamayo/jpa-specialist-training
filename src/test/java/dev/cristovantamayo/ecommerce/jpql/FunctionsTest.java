e19ba35b911ce775c3a7f45cad8407de3faa698c
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-13 18:10:42 -0300

    Date Functions

--- src/test/java/dev/cristovantamayo/ecommerce/jpql/FunctionsTest.java
+++ src/test/java/dev/cristovantamayo/ecommerce/jpql/FunctionsTest.java
@@ -1,20 +1,20 @@
 package dev.cristovantamayo.ecommerce.jpql;
 
 import dev.cristovantamayo.ecommerce.EntityManagerTest;
-import dev.cristovantamayo.ecommerce.model.Client;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.TimeZone;
 
 import static java.lang.String.format;
 
-public class StringFunctionsTest extends EntityManagerTest {
+public class FunctionsTest extends EntityManagerTest {
 
     // concat, length, locate, substring, lower, upper, trim
     @Test
-    public void applyFunction() {
+    public void applyStringFunctions() {
         final String jpql = "select c.name, concat('Category: ', c.name) from Category c";
         final String jpqlLength = "select c.name, length(c.name) from Category c";
         final String jpqlLocate = "select c.name, locate('a', c.name) from Category c";
@@ -31,5 +31,22 @@ public class StringFunctionsTest extends EntityManagerTest {
         List<Object[]> list = typedQuery.getResultList();
         Assertions.assertFalse(list.isEmpty());
         list.forEach(arr -> System.out.println(format("%s, %s, %s", arr[0], arr[1], arr[2]!=null?arr[2]:"")));
+    }
+
+    @Test
+    public void applyDateFunctions() {
+        //TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+        final String jpqlConditional = "select current_date, current_time, current_timestamp from Purchase p " +
+                "where p.purchaseDate < current_date";
+
+        final String jpqlYEAR = "select year(p.purchaseDate), month(p.purchaseDate), day(p.purchaseDate) from Purchase p";
+
+        final String jpqlHour = "select hour(p.purchaseDate), minute(p.purchaseDate), second(p.purchaseDate) from Purchase p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpqlHour, Object[].class);
+        List<Object[]> list = typedQuery.getResultList();
+        Assertions.assertFalse(list.isEmpty());
+        list.forEach(arr -> System.out.println(format("%s | %s | %s", arr[0], arr[1], arr[2]!=null?arr[2]:"")));
     }
 }
