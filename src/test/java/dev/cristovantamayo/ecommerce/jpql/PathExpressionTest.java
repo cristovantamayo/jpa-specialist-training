2738cdeacda265f23c9cc58edfd58ddc69c81e27
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-13 09:13:56 -0300

    Path Expression Exercice

--- src/test/java/dev/cristovantamayo/ecommerce/jpql/PathExpressionTest.java
+++ src/test/java/dev/cristovantamayo/ecommerce/jpql/PathExpressionTest.java
@@ -1,6 +1,7 @@
 package dev.cristovantamayo.ecommerce.jpql;
 
 import dev.cristovantamayo.ecommerce.EntityManagerTest;
+import dev.cristovantamayo.ecommerce.model.Purchase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,19 @@ import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class PathExpressionTest extends EntityManagerTest {
+
+    @Test
+    public void searchPurchaseWithASpecificProduct() {
+        final String jpql = "select p from Purchase p " +
+                "join p.purchaseItems i WHERE i.product.id = 1";
+
+        TypedQuery<Purchase> typedQuery = entityManager.createQuery(jpql, Purchase.class);
+        List<Purchase> purchases = typedQuery.getResultList();
+        Assertions.assertFalse(purchases.isEmpty());
+        purchases.forEach(p ->
+                p.getPurchaseItems().forEach(i ->
+                        System.out.println(i.getProduct().getName())));
+    }
 
     @Test
     public void usePathExpressions() {
