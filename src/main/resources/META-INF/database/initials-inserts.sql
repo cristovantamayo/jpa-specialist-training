cbf758b33b2bb43472c6bec409472b669b496b66
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-15 09:00:59 -0300

    Case Clause

--- src/main/resources/META-INF/database/initials-inserts.sql
+++ src/main/resources/META-INF/database/initials-inserts.sql
@@ -8,10 +8,10 @@ insert into client (id, name, cpf) values (2, 'Marcos Mariano', '222222222');
 insert into client_detail (client_id, gender, birth_date) values (1, 'MAN', date_sub(sysdate(), interval 27 year));
 insert into client_detail (client_id, gender, birth_date) values (2, 'MAN', date_sub(sysdate(), interval 30 year));
 
-insert into purchase (id, client_id, purchase_date, total, status) values (1, 1, date_sub(sysdate(), interval 5 day), 2398.0, 'WAITING');
+insert into purchase (id, client_id, purchase_date, total, status) values (1, 1, date_sub(sysdate(), interval 5 day), 2398.0, 'PAID_OUT');
 insert into purchase (id, client_id, purchase_date, total, status) values (2, 1, sysdate(), 499.0, 'WAITING');
 insert into purchase (id, client_id, purchase_date, total, status) values (3, 1, date_sub(sysdate(), interval 2 day), 3500.0, 'PAID_OUT');
-insert into purchase (id, client_id, purchase_date, total, status) values (4, 2, date_sub(sysdate(), interval 2 day), 499.0, 'PAID_OUT');
+insert into purchase (id, client_id, purchase_date, total, status) values (4, 2, date_sub(sysdate(), interval 2 day), 499.0, 'WAITING');
 
 insert into purchase_item (purchase_id, product_id, product_price, quantity) values (1, 1, 499, 2);
 insert into purchase_item (purchase_id, product_id, product_price, quantity) values (1, 3, 1400, 1);
@@ -19,8 +19,10 @@ insert into purchase_item (purchase_id, product_id, product_price, quantity) val
 insert into purchase_item (purchase_id, product_id, product_price, quantity) values (3, 4, 3500, 1);
 insert into purchase_item (purchase_id, product_id, product_price, quantity) values (4, 1, 499, 1);
 
-insert into payment (purchase_id, payment_status, card_number, payment_type) values (2, 'IN_PROCESS', '123', 'CredCard');
---insert into payment_by_credcard (purchase_id, card_number) values (2, '123')
+insert into payment (purchase_id, payment_status, payment_type, card_number, bar_code) values (1, 'RECEIVED', 'CredCard', '0123', null);
+insert into payment (purchase_id, payment_status, payment_type, card_number, bar_code) values (2, 'IN_PROCESS', 'CredCard', '4567', null);
+insert into payment (purchase_id, payment_status, payment_type, card_number, bar_code) values (3, 'RECEIVED', 'Ticket', null, '8910');
+insert into payment (purchase_id, payment_status, payment_type, card_number, bar_code) values (4, 'IN_PROCESS', 'CredCard', '1112', null);
 
 insert into invoice (purchase_id, xml, issue_date) VALUES (2, "<?xml></xml>", date_sub(sysdate(), interval 1 day));
 
