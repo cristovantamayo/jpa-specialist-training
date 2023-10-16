090b6fccfe4f1206567a5046c52c99fd77969b9c
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-16 18:35:31 -0300

    Load Queries from XML file

--- src/test/java/dev/cristovantamayo/ecommerce/jpql/NamedQueryTest.java
+++ src/test/java/dev/cristovantamayo/ecommerce/jpql/NamedQueryTest.java
@@ -2,6 +2,7 @@ package dev.cristovantamayo.ecommerce.jpql;
 
 import dev.cristovantamayo.ecommerce.EntityManagerTest;
 import dev.cristovantamayo.ecommerce.model.Product;
+import dev.cristovantamayo.ecommerce.model.Purchase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,28 @@ import java.util.List;
 import static java.lang.String.format;
 
 public class NamedQueryTest extends EntityManagerTest {
+
+    @Test
+    public void executeSearchFromXmlPersonalProductFile() {
+        TypedQuery<Product> typedQuery = entityManager.createNamedQuery("Product.list", Product.class);
+        List<Product> products = typedQuery.getResultList();
+        Assertions.assertFalse(products.isEmpty());
+        products.forEach(p -> System.out.println(format("ID: %s, PRODUCT: %s, PRICE: %s", p.getId(), p.getName(), p.getPrice())));
+    }
+
+    @Test
+    public void executeSearchFromXmlPersonalFile() {
+        TypedQuery<Purchase> typedQuery = entityManager.createNamedQuery("Purchase.listing", Purchase.class);
+        List<Purchase> purchases = typedQuery.getResultList();
+        Assertions.assertFalse(purchases.isEmpty());
+    }
+
+    @Test
+    public void executeSearchFromXmlFile() {
+        TypedQuery<Purchase> typedQuery = entityManager.createNamedQuery("Purchase.list", Purchase.class);
+        List<Purchase> purchases = typedQuery.getResultList();
+        Assertions.assertFalse(purchases.isEmpty());
+    }
 
     @Test
     public void executeSearch() {
