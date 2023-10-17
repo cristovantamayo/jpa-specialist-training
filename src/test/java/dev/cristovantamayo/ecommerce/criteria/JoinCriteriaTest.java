be407be462a32e571cec3040fffdb6d4ff7653ee
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-17 01:20:55 -0300

    Criteria Join Fetch

--- src/test/java/dev/cristovantamayo/ecommerce/criteria/JoinCriteriaTest.java
+++ src/test/java/dev/cristovantamayo/ecommerce/criteria/JoinCriteriaTest.java
@@ -12,6 +12,45 @@ import java.util.List;
 public class JoinCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void doCriteriaJoinFetchAsJoin() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Client> criteriaQuery = criteriaBuilder.createQuery(Client.class);
+        Root<Purchase> root = criteriaQuery.from(Purchase.class);
+        //root.fetch("invoice", JoinType.LEFT);
+        //root.fetch("payment", JoinType.LEFT);
+        Join<Purchase, Client> joinClient = root.join("client");
+               // (Join<Purchase, Client>) root.<Purchase, Client>fetch("client");
+
+
+        criteriaQuery.select(joinClient);
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<Client> typedQuery = entityManager.createQuery(criteriaQuery);
+        Client client = typedQuery.getSingleResult();
+        Assertions.assertNotNull(client);
+        System.out.println(client.getName());
+    }
+
+    @Test
+    public void doCriteriaJoinFetch() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Purchase> criteriaQuery = criteriaBuilder.createQuery(Purchase.class);
+        Root<Purchase> root = criteriaQuery.from(Purchase.class);
+        root.fetch("client");
+        root.fetch("invoice", JoinType.LEFT);
+        root.fetch("payment", JoinType.LEFT);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<Purchase> typedQuery = entityManager.createQuery(criteriaQuery);
+        Purchase purchase = typedQuery.getSingleResult();
+        Assertions.assertNotNull(purchase);
+    }
+
+    @Test
     public void doCriteriaLeftOuterJoin() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Purchase> criteriaQuery = criteriaBuilder.createQuery(Purchase.class);
