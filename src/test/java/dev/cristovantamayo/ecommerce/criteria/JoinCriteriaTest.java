f73a3d1aec3f73f689968a0ad386b276206ca464
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-17 00:09:05 -0300

    Criteria Joins

---
+++ src/test/java/dev/cristovantamayo/ecommerce/criteria/JoinCriteriaTest.java
@@ -0,0 +1,46 @@
+package dev.cristovantamayo.ecommerce.criteria;
+
+import dev.cristovantamayo.ecommerce.EntityManagerTest;
+import dev.cristovantamayo.ecommerce.model.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class JoinCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void doCriteriaJoin() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Purchase> criteriaQuery = criteriaBuilder.createQuery(Purchase.class);
+        Root<Purchase> root = criteriaQuery.from(Purchase.class);
+        Join<Purchase, Payment> join = root.join("payment");
+        // Join<Purchase, PurchaseItem> joinItems = root.join("purchaseItems");
+        // Join<PurchaseItem, Product> joinPurchaseItem = joinItems.join("product");
+
+        criteriaQuery.select(root);
+
+        TypedQuery<Purchase> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Purchase> purchases = typedQuery.getResultList();
+        Assertions.assertTrue(purchases.size() == 4);
+
+
+
+        CriteriaQuery<Payment> criteriaQuery2 = criteriaBuilder.createQuery(Payment.class);
+        Root<Purchase> root2 = criteriaQuery2.from(Purchase.class);
+        Join<Purchase, Payment> joinPayment = root2.join("payment");
+
+        criteriaQuery2.select(joinPayment);
+        criteriaQuery2.where(criteriaBuilder
+                .equal(joinPayment.get("paymentStatus"), PaymentStatus.IN_PROCESS));
+
+        TypedQuery<Payment> typedQuery2 = entityManager.createQuery(criteriaQuery2);
+        List<Payment> payments = typedQuery2.getResultList();
+        Assertions.assertTrue(payments.size() == 2);
+    }
+}
