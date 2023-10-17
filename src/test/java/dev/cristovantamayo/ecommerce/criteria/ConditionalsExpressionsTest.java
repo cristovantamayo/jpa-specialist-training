e5d235680d449b1a194c42c82ec090bcf9a9f56f
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-17 14:56:31 -0300

    Criteria Check NULLs and Emptys

---
+++ src/test/java/dev/cristovantamayo/ecommerce/criteria/ConditionalsExpressionsTest.java
@@ -0,0 +1,68 @@
+package dev.cristovantamayo.ecommerce.criteria;
+
+import dev.cristovantamayo.ecommerce.EntityManagerTest;
+import dev.cristovantamayo.ecommerce.model.Client;
+import dev.cristovantamayo.ecommerce.model.Client_;
+import dev.cristovantamayo.ecommerce.model.Product;
+import dev.cristovantamayo.ecommerce.model.Product_;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.lang.String.format;
+
+public class ConditionalsExpressionsTest extends EntityManagerTest {
+
+    @Test
+    public void useIsNUll() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+        Root<Product> root = criteriaQuery.from(Product.class);
+
+        criteriaQuery.select(root);
+        // criteriaQuery.where(root.get(Product_.PHOTO).isNull());
+        // criteriaQuery.where(root.get(Product_.PHOTO).isNotNull());
+        criteriaQuery.where(criteriaBuilder.isNull(root.get(Product_.PHOTO)));
+        // criteriaQuery.where(criteriaBuilder.isNotNull(root.get(Product_.PHOTO)));
+
+        TypedQuery<Product> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Product> products = typedQuery.getResultList();
+        Assertions.assertFalse(products.isEmpty());
+        products.forEach(p -> System.out.println(format("ID: %s, PRODUCT: %s, PRICE: %s", p.getId(), p.getName(), p.getPrice())));
+    }
+
+    @Test
+    public void useIsEmpty() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+        Root<Product> root = criteriaQuery.from(Product.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.isEmpty(root.get(Product_.CATEGORIES)));
+
+        TypedQuery<Product> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Product> products = typedQuery.getResultList();
+        Assertions.assertFalse(products.isEmpty());
+        products.forEach(p -> System.out.println(format("ID: %s, PRODUCT: %s, PRICE: %s", p.getId(), p.getName(), p.getPrice())));
+    }
+
+    @Test
+    public void useConditionalLikeExpression() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Client> criteriaQuery = criteriaBuilder.createQuery(Client.class);
+        Root<Client> root = criteriaQuery.from(Client.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.like(root.get(Client_.name), "%a%"));
+
+        TypedQuery<Client> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Client> clients = typedQuery.getResultList();
+        Assertions.assertFalse(clients.isEmpty());
+        clients.forEach(c -> System.out.println(c.getName()));
+    }
+}
