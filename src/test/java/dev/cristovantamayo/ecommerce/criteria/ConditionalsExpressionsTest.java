61154543ef30a41665198a093a59b8e8996accdb
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-17 15:10:27 -0300

    Major (greaterThen) and Minor (lessThen) Criteria

--- src/test/java/dev/cristovantamayo/ecommerce/criteria/ConditionalsExpressionsTest.java
+++ src/test/java/dev/cristovantamayo/ecommerce/criteria/ConditionalsExpressionsTest.java
@@ -12,11 +12,60 @@ import jakarta.persistence.criteria.Root;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static java.lang.String.format;
 
 public class ConditionalsExpressionsTest extends EntityManagerTest {
+
+    @Test
+    public void useMajorAndMinorExpression() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+        Root<Product> root = criteriaQuery.from(Product.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(
+                criteriaBuilder.greaterThan(root.get(Product_.PRICE), new BigDecimal(799)),
+                criteriaBuilder.lessThan(root.get(Product_.PRICE), new BigDecimal(3500))
+        );
+
+        TypedQuery<Product> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Product> products = typedQuery.getResultList();
+        Assertions.assertFalse(products.isEmpty());
+        products.forEach(p -> System.out.println(format("ID: %s, PRODUCT: %s, PRICE: %s", p.getId(), p.getName(), p.getPrice())));
+    }
+
+    @Test
+    public void useMinorExpression() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+        Root<Product> root = criteriaQuery.from(Product.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.lessThan(root.get(Product_.PRICE), new BigDecimal(799)));
+
+        TypedQuery<Product> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Product> products = typedQuery.getResultList();
+        Assertions.assertFalse(products.isEmpty());
+        products.forEach(p -> System.out.println(format("ID: %s, PRODUCT: %s, PRICE: %s", p.getId(), p.getName(), p.getPrice())));
+    }
+
+    @Test
+    public void useMajorExpression() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+        Root<Product> root = criteriaQuery.from(Product.class);
+
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.greaterThanOrEqualTo(root.get(Product_.PRICE), new BigDecimal(2200)));
+
+        TypedQuery<Product> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Product> products = typedQuery.getResultList();
+        Assertions.assertFalse(products.isEmpty());
+        products.forEach(p -> System.out.println(format("ID: %s, PRODUCT: %s, PRICE: %s", p.getId(), p.getName(), p.getPrice())));
+    }
 
     @Test
     public void useIsNUll() {
