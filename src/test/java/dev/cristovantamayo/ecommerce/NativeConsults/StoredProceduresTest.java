20f368f3c5129fd46e25823f1aa8f20779820d8b
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-27 08:31:22 -0300

    Native Stored Procedure returns List

--- src/test/java/dev/cristovantamayo/ecommerce/NativeConsults/StoredProceduresTest.java
+++ src/test/java/dev/cristovantamayo/ecommerce/NativeConsults/StoredProceduresTest.java
@@ -1,13 +1,31 @@
 package dev.cristovantamayo.ecommerce.NativeConsults;
 
 import dev.cristovantamayo.ecommerce.EntityManagerTest;
+import dev.cristovantamayo.ecommerce.model.Client;
 import jakarta.persistence.NamedStoredProcedureQuery;
 import jakarta.persistence.ParameterMode;
 import jakarta.persistence.StoredProcedureQuery;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class StoredProceduresTest extends EntityManagerTest {
+
+    @Test
+    public void receiveListFormProcedure() {
+        StoredProcedureQuery storedProcedureQuery = entityManager
+                .createStoredProcedureQuery("sold_above_average", Client.class);
+
+        storedProcedureQuery.registerStoredProcedureParameter(
+                "p_year", Integer.class, ParameterMode.IN);
+
+        storedProcedureQuery.setParameter("p_year", 2023);
+
+        List<Client> clients = storedProcedureQuery.getResultList();
+
+        Assertions.assertFalse(clients.isEmpty());
+    }
 
     @Test
     public void useParametersInAndOut() {
