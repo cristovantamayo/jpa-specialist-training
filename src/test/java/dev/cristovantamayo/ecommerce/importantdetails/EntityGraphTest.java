df52e60b24a22259ce9ed14c6eb85383e4872364
cristovantamayo <cristovan.tamayo@gmail.com>
2023-10-30 07:55:06 -0300

    SubGraph

--- src/test/java/dev/cristovantamayo/ecommerce/importantdetails/EntityGraphTest.java
+++ src/test/java/dev/cristovantamayo/ecommerce/importantdetails/EntityGraphTest.java
@@ -1,8 +1,10 @@
 package dev.cristovantamayo.ecommerce.importantdetails;
 
 import dev.cristovantamayo.ecommerce.EntityManagerTest;
+import dev.cristovantamayo.ecommerce.model.Client;
 import dev.cristovantamayo.ecommerce.model.Purchase;
 import jakarta.persistence.EntityGraph;
+import org.hibernate.graph.SubGraph;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +13,25 @@ import java.util.List;
 import java.util.Map;
 
 public class EntityGraphTest extends EntityManagerTest {
+
+    @Test
+    public void LookingForEssentialsAttributesOfPurchase03 () {
+
+        EntityGraph<Purchase> entityGraph =
+                entityManager.createEntityGraph(Purchase.class);
+
+        entityGraph.addAttributeNodes("purchaseDate", "status", "total", "invoice");
+
+        SubGraph<Client> clientSubGraph = (SubGraph<Client>) entityGraph.addSubgraph("client", Client.class);
+        clientSubGraph.addAttributeNodes("name", "cpf");
+
+        List<Purchase> purchases = entityManager.createQuery("select p from Purchase p where p.id = 1")
+                .setHint("javax.persistence.fetchgraph", entityGraph)
+                .getResultList();
+
+        Assertions.assertFalse(purchases.isEmpty());
+
+    }
 
     @Test
     public void LookingForEssentialsAttributesOfPurchase02 () {
